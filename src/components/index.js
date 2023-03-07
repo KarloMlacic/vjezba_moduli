300902f18180b9846a557ba5df5f84e9d4c11566
Karlo Mlačić <karlomlacic90@gmail.com>
2023-03-07 19:48:19 +0100

    C

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export {default as NekaKompa} from "./Nekakompa";
 export {default as PrvaKompa} from "./Prvakompa";
-export {default as CetvrtaKompa, DrugaKompa, TrecaKompa} from "./Drugakompa"
+export {default as CetvrtaKompa, DrugaKompa, TrecaKompa} from "./Drugakompa";
